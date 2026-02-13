22765066a85483b1d205d826cc9b376f14d1bc0d
rishabh9112 <rishabhagrawal9112@gmail.com>
2026-02-13 11:41:46 +0530

    Add itsdangerous dependency for session middleware

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.routers import auth
+from starlette.middleware.sessions import SessionMiddleware
 
+from .config import settings
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -11,7 +13,10 @@ origins = [
     "http://localhost:5173",
     "https://quiz-frontend-kids.vercel.app",
 ]
-
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=settings.SECRET_KEY
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,6 +24,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    
 )
 
 app.include_router(auth.router, prefix="/api/v1")
